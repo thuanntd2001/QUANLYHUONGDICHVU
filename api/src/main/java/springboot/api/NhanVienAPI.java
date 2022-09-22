a2bb7cec2a06241843a05893f6edc679984c745f
thuanntd2001 <thiensudanduong12@gmail.com>
2022-09-22 08:59:28 +0700

    add create nhanvien

--- api/src/main/java/springboot/api/NhanVienAPI.java
+++ api/src/main/java/springboot/api/NhanVienAPI.java
@@ -1,5 +1,6 @@
 package springboot.api;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import springboot.dto.NhanVienDTO;
 import springboot.dto.UserDTO;
 import springboot.entity.BanEntity;
 import springboot.entity.NhanVienEntity;
@@ -37,11 +39,40 @@ public class NhanVienAPI {
 	}
 
 	@PostMapping(value="/nhanvien")
-	public UserDTO createNV(@RequestBody UserDTO model) {
+	public String createNV(@RequestBody NhanVienDTO model) {
+//		if (repo.findById(model.getMaNV()) == null) {
+//			NhanVienEntity esave = null;
+//			UserModel user = (UserModel) session.getAttribute("USERMODEL");
+		NhanVienEntity save= new NhanVienEntity();
+		NhanVienEntity check = null;
+			try {
+				save.setCmnd(model.getCmnd());
+				save.setDaNghi(model.getDaNghi());
+				save.setDiaChi(model.getDiaChi());
+				save.setGioiTinh(model.getGioiTinh());
+				save.setHoTen(model.getHoTen());
+				save.setNgaySinh(model.getNgaySinh());
+				save.setLuong(model.getLuong());
+				save.setNgayVaoLam(model.getNgayVaoLam());
+				save.setSdt(model.getSdt());
+			
+				check = repo.save(save);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.print(model.getCmnd());
+				System.out.print(model.getHoTen());
+				return "01";
+			}
+			if (check == null) {
+				
+				return "02";
+			}
+			return "00";
+		} 
 
-		return model;
+		
+	
 
-	}
 	
 	@PutMapping(value="/nhanvien")
 	public UserDTO updateNV(@RequestBody UserDTO model) {
