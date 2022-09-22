14987d5023830f72f37079f77b53f55fa57ba25c
thuanntd2001 <thiensudanduong12@gmail.com>
2022-09-22 16:36:59 +0700

    add delete nhanvien methods

--- api/src/main/java/springboot/api/NhanVienAPI.java
+++ api/src/main/java/springboot/api/NhanVienAPI.java
@@ -1,6 +1,5 @@
 package springboot.api;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,10 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import springboot.dto.NhanVienDTO;
-import springboot.dto.UserDTO;
-import springboot.entity.BanEntity;
 import springboot.entity.NhanVienEntity;
-import springboot.repository.BanRepository;
 import springboot.repository.NhanVienRepository;
 
 @RestController
@@ -26,59 +22,54 @@ public class NhanVienAPI {
 
 	@GetMapping("/nhanvien")
 	public List<NhanVienEntity> getNV() {
-		 List<NhanVienEntity> list=repo.findAll();
-		 for (NhanVienEntity item:list)
-		 {
-			
-			 item.setChiPhi(null);
-			 item.setHoadon(null);
-			 item.setUserTB(null);
-		 }
-		 System.out.print(list.size());
+		List<NhanVienEntity> list = repo.findAll();
+		for (NhanVienEntity item : list) {
+
+			item.setChiPhi(null);
+			item.setHoadon(null);
+			item.setUserTB(null);
+		}
+		System.out.print(list.size());
 		return list;
 
 	}
 
-	@PostMapping(value="/nhanvien")
+	@PostMapping(value = "/nhanvien")
 	public String createNV(@RequestBody NhanVienDTO model) {
 
-		NhanVienEntity save= new NhanVienEntity();
+		NhanVienEntity save = new NhanVienEntity();
 		NhanVienEntity check = null;
-			try {
-				save.setCmnd(model.getCmnd());
-				save.setDaNghi(model.getDaNghi());
-				save.setDiaChi(model.getDiaChi());
-				save.setGioiTinh(model.getGioiTinh());
-				save.setHoTen(model.getHoTen());
-				save.setNgaySinh(model.getNgaySinh());
-				save.setLuong(model.getLuong());
-				save.setNgayVaoLam(model.getNgayVaoLam());
-				save.setSdt(model.getSdt());
-			
-				check = repo.save(save);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.print(model.getCmnd());
-				System.out.print(model.getHoTen());
-				return "01";
-			}
-			if (check == null) {
-				
-				return "02";
-			}
-			return "00";
-		} 
+		try {
+			save.setCmnd(model.getCmnd());
+			save.setDaNghi(model.getDaNghi());
+			save.setDiaChi(model.getDiaChi());
+			save.setGioiTinh(model.getGioiTinh());
+			save.setHoTen(model.getHoTen());
+			save.setNgaySinh(model.getNgaySinh());
+			save.setLuong(model.getLuong());
+			save.setNgayVaoLam(model.getNgayVaoLam());
+			save.setSdt(model.getSdt());
 
-		
-	
+			check = repo.save(save);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.print(model.getCmnd());
+			System.out.print(model.getHoTen());
+			return "01";
+		}
+		if (check == null) {
 
-	
-	@PutMapping(value="/nhanvien")
+			return "02";
+		}
+		return "00";
+	}
+
+	@PutMapping(value = "/nhanvien")
 	public String updateNV(@RequestBody NhanVienDTO model) {
 
 		Optional<NhanVienEntity> nvoption = repo.findById(model.getMaNV());
 		if (nvoption.isEmpty()) {
-			
+
 			System.out.print("ko tồn tại nv");
 			return "404";
 		}
@@ -86,11 +77,10 @@ public class NhanVienAPI {
 		else {
 			System.out.print("tồn tại nv");
 			NhanVienEntity save = nvoption.get();
-			NhanVienEntity check=null;
+			NhanVienEntity check = null;
 			try {
-				
+
 				save.setCmnd(model.getCmnd());
-				save.setDaNghi(model.getDaNghi());
 				save.setDiaChi(model.getDiaChi());
 				save.setGioiTinh(model.getGioiTinh());
 				save.setHoTen(model.getHoTen());
@@ -98,26 +88,49 @@ public class NhanVienAPI {
 				save.setLuong(model.getLuong());
 				save.setNgayVaoLam(model.getNgayVaoLam());
 				save.setSdt(model.getSdt());
-				check=repo.save(save);
+				check = repo.save(save);
 			} catch (Exception e) {
 				e.printStackTrace();
 				return "01";
 			}
 
-			
 			if (check == null) {
 				return "02";
 			}
 			return "00";
 		}
-		
 
 	}
-	
-	@DeleteMapping(value="/nhanvien")
-	public void deleteNV(@RequestBody Long[] ids) {
 
-		
+	@DeleteMapping(value = "/nhanvien")
+	public String deleteNV(@RequestBody NhanVienDTO ids) {
+		Optional<NhanVienEntity> nvoption = repo.findById(ids.getMaNV());
+		if (nvoption.isEmpty()) {
+
+			System.out.print("ko tồn tại nv");
+			return "404";
+		}
+
+		else {
+			System.out.print("tồn tại nv");
+			NhanVienEntity save = nvoption.get();
+			NhanVienEntity check = null;
+			try {
+
+				save.setDaNghi(true);
+				check = repo.save(save);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "01";
+			}
+
+			if (check == null) {
+				return "02";
+			}
+			return "00";
+		}
 
 	}
+
 }
